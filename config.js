e105ed27807501d965602b4d526a3ba3d9e3fad7
Sven Hergenhahn <sven.hergenhahn@dm.de>
2022-06-15 07:54:35 +0200

    try to make it inot a minimal reproduction

--- config.js
+++ config.js
@@ -1,25 +1,5 @@
 module.exports = {
   platform: 'github',
-  schedule: ["every weekday"],
-  logFileLevel: process.env.RENOVATE_LOG_FILE_LEVEL,
-  logFile: process.env.LOG_FILE,
-  includeForks: true,
-  dependencyDashboard: false,
-  platformAutomerge: true,
-  onboarding: true,
-  extends: [
-    ":semanticPrefixFixDepsChoreOthers",
-    ":ignoreModulesAndTests",
-    ":autodetectPinVersions",
-    "group:monorepos",
-    "group:recommended",
-    "workarounds:all",
-    ":rebaseStalePrs",
-    ":disableDependencyDashboard"
-  ],
-  prConcurrentLimit: 0,
-  prHourlyLimit: 0,
-  branchConcurrentLimit: 0,
   labels: ["dependencies"],
   packageRules: [
     {
--- renovate_wrapper.sh
+++
@@ -1,6 +0,0 @@
-export RENOVATE_CONFIG_FILE=$(pwd)/renovate-test-2.js  
-export LOG_LEVEL=debug
-export RENOVATE_LOG_FILE=renovate.log
-export RENOVATE_LOG_FILE_LEVEL=debug
-
-renovate --print-config
